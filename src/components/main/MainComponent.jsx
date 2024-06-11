dad69912fa260831424ffc69aed59bf48dc3ad6d
haelim <gpfla7375@gmail.com>
2024-06-11 12:06:58 +0900

    24_06_11 12:07 메인  페이지 이동 component 폴더 아래 MainComponent 추가 next.config.js 추가

---
+++ src/components/main/MainComponent.jsx
@@ -0,0 +1,14 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+
+const MainComponent = () => {
+    return (
+        <>
+            <h1>main 화면</h1>
+            <div>
+                테스트
+            </div>
+        </>
+    );
+};
+export default MainComponent;
