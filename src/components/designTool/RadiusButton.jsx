bbf448c96c9ea0d3d12ca8bd148d849baec79f37
RhysBlair <hanmanman12345@gmail.com>
2024-06-13 16:28:48 +0900

    네비게이션 슬라이드 구현

--- src/components/designTool/RadiusButton.jsx
+++ src/components/designTool/RadiusButton.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const RadiusButton = ({ padding, color, text, onClick, fontSize }) => {
+const RadiusButton = ({
+  padding,
+  color,
+  text,
+  onClick,
+  fontSize,
+  borderRadius,
+}) => {
   const buttonStyles = {
     padding: padding || "12px 24px",
     backgroundColor: color,
     border: "none",
-    borderRadius: "30px",
+    borderRadius: borderRadius || "10px",
     color: "#fff",
     cursor: "pointer",
     fontWeight: "600",
@@ -26,13 +33,17 @@ RadiusButton.propTypes = {
   text: PropTypes.string.isRequired,
   onClick: PropTypes.func,
   fontSize: PropTypes.string,
+  borderRadius: PropTypes.string,
 };
 
 RadiusButton.defaultProps = {
   padding: "12px 24px",
   color: "#007bff",
-  onClick: () => {},
+  onClick: () => {
+    alert("뭘봐");
+  },
   fontSize: "14px",
+  borderRadius: "10px",
 };
 
 export default RadiusButton;
