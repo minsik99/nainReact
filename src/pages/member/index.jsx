6d8cb1e22713b9ee6f604862c9b25ac61a8d1a58
LEON\wjstk <wjstkddn1990@naver.com>
2024-06-26 12:58:08 +0900

    24_06_26_12:57 충돌 수정

--- src/pages/member/index.jsx
+++ src/pages/member/index.jsx
@@ -1,14 +1,11 @@
 // index.jsx
 
 // eslint-disable-next-line no-unused-vars
-import SignUpForm from "./signUpForm";
+import SignUpForm from "/src/components/member/signup";
 import styles from "../../styles/member/member.module.css";
 
-
 const Index = () => {
-    return (
-            <SignUpForm/>
-    );
+  return <SignUpForm />;
 };
 
 export default Index;
